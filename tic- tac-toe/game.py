a4d1d0f670fd04aee5c07a5241b86258ba6a47dc
Leonelta <ketchakaren@gmail.com>
2021-04-20 09:34:05 +0200

    create current_winner that will keep track of the current winner

--- tic- tac-toe/game.py
+++ tic- tac-toe/game.py
@@ -1,3 +1,4 @@
 class TicTacToe:
     def __init__(self):
-        self.board = ['' for _ in range(9)]
+        self.board = ['' for _ in range(9)] # we will use a single list to rep 3*3 board
+        self.current_winner = None
