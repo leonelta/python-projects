1e34f192a56a09c7712bdc4407940986c483fbea
Leonelta <ketchakaren@gmail.com>
2021-04-20 09:28:37 +0200

    cretae class TicTacToe and initialize it with self

--- tic- tac-toe/player.py
+++ tic- tac-toe/player.py
@@ -18,4 +18,8 @@ class RandomComputerPlayer(Player):
         pass
 
 class HumanPlayer(Player):
-    
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        pass
