5b2f08ea50f80f1ac0f4f0653bf0553229ee438a
Leonelta <ketchakaren@gmail.com>
2021-04-20 09:20:47 +0200

    we define a function get_move so that the player can get  his next move

--- tic- tac-toe/player.py
+++ tic- tac-toe/player.py
@@ -1,6 +1,10 @@
 import math
 import random
 
-class player:
+class Player:
     def __init__(self, letter):
-        
+        # letter is x or o
+        self.letter = letter
+
+    #we want all players to get their next move given a game
+    def get_move(self, game):
