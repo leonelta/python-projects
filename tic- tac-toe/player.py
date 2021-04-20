ae92f3307b3cc98e8a06824c4c3f096dddb05b75
Leonelta <ketchakaren@gmail.com>
2021-04-20 09:25:58 +0200

    still create another class HumanPlayer which inherited from the superclass Player

--- tic- tac-toe/player.py
+++ tic- tac-toe/player.py
@@ -11,3 +11,11 @@ class Player:
         pass
 
 class RandomComputerPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        pass
+
+class HumanPlayer(Player):
+    
