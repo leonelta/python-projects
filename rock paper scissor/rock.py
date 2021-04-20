fdae42efe4930a1451f26200a78090b1e08dac51
Leonelta <ketchakaren@gmail.com>
2021-04-20 09:10:22 +0200

    going to import math and random so as to use them without any error

--- rock paper scissor/rock.py
+++ rock paper scissor/rock.py
@@ -1,13 +1,24 @@
 import random
 
 def play():
-    user = input("'r'for rock, 'p' for paper, 's' for scissors")
+    user = input("What's your choice? 'r'for rock, 'p' for paper, 's' for scissors")
     computer = random.choice(['r', 'p', 's'])
 
     if user == computer:
-        return 'tie'
+        return ' It\'s a tie'
 
     # r > s, s > p, p > r
+    if is_win(user, computer):
+        return 'You won!'
 
-def is_win(player, opponent)
-    
+    return 'You lost!'
+
+def is_win(player, opponent):
+#return true if player wins
+ # r > s, s > p, p > r
+
+ if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p')\
+     or (player == 'p' and opponent == 'r'):
+     return True
+
+print(play())
