b89c39c041923e53531a1e45c6e2ba038c638045
Leonelta <ketchakaren@gmail.com>
2021-04-22 12:08:58 +0200

    define a function that will help us in getting rows

--- tic- tac-toe/game.py
+++ tic- tac-toe/game.py
@@ -2,3 +2,10 @@ class TicTacToe:
     def __init__(self):
         self.board = ['' for _ in range(9)] # we will use a single list to rep 3*3 board
         self.current_winner = None
+
+    def print_board(self):
+        #This is just getting the rows
+        for row in [self.board[i*3:(i*1)*3] for i in range(3)]:
+            print('|' + '|'.join(row) + ' |')
+            
+
