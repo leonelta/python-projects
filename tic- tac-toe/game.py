c11a7e2fbb647502c0dc65aef43cb34d19dc43da
Leonelta <ketchakaren@gmail.com>
2021-04-26 21:41:34 +0200

    tiny break to make things a little easier to read

--- tic- tac-toe/game.py
+++ tic- tac-toe/game.py
@@ -1,3 +1,6 @@
+import time
+from player import HumanPlayer, RandomComputerPlayer
+
 class TicTacToe:
     def __init__(self):
         self.board = ['' for _ in range(9)] # we will use a single list to rep 3*3 board
@@ -47,7 +50,21 @@ class TicTacToe:
         column = [self.board[col_ind+i*3] for i in range(3)]
         if all([spot == letter for spot in column]):
             return True
-        
+
+        #check diagonals
+        # but only if the square is an even number (0, 2, 4, 6, 8)  
+        # these are the only moves possible to win a diagonal
+        if square % 2 == 0:
+            diagonal1 = [self.board[i] for i in [0, 4, 8]] #left to right diagonal
+            if all([spot == letter for spot in diagonal1]):
+                return True
+            diagonal2 = [self.board[i] for i in [2, 4, 6]] #right to left diagonal
+            if all([spot == letter for spot in diagonal2]):
+                return True
+
+                return False
+
+
 def play(game, x_player, o_player, print_game=True):
     if print_game:
         game.print_board_nums()
@@ -74,12 +91,15 @@ def play(game, x_player, o_player, print_game=True):
 
             #after we made our move, we need to alternate letters
             letter = 'O' if letter == 'X' else 'X'
-           # if letter == 'X':
-           #     letter = 'O'
-           # else:
-           #     letter = 'X'
-
-             #BUT WAIT WHAT IF WE WON??
+           
+           #tiny break to make things littleeasier to read
+           time.sleep(0.8)
 
         if print_game:
             print('It\'s a tie!')
+
+if __name__ == '__main__':
+    x_player = HumanPlayer('X')
+    o_player = RandomComputerPlayer('O')
+    t = TicTacToe()
+    play(t, x_player, o_player, print_game=True)
