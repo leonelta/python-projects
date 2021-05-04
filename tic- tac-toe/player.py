f39ee12d1f38a3e5cbbcb792e878f6a524678caf
Leonelta <ketchakaren@gmail.com>
2021-05-04 09:12:17 +0200

    if noempty square no one has won

--- tic- tac-toe/player.py
+++ tic- tac-toe/player.py
@@ -51,5 +51,17 @@ def GeniusComputerPlayer(Player):
         return square
 
     def minimax(self, state, player):
-        max_player = self.letter 
+        max_player = self.letter #yourself
+        other_player = 'O' if player == 'X' else 'X'
+
+
+        if state.current_winner  == other_player:
+            return('poistion': None,
+                   'score': 1*(state.num_empty_square() + 1) if other_player == max_player else -1 *(
+                      state.num_empty_square() + 1 )
+                   )
+
+        elif not state.empty_squares():
+            return('position': None, 'score': 0)
+
 
