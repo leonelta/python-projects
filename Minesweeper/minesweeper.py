5ee468eabd9a03f1b4a576b88956da4fd3885421
leonelta <ketchakaren@gmail.com>
2021-08-03 19:56:34 +0200

    we the row, col into this set declared

--- Minesweeper/minesweeper.py
+++ Minesweeper/minesweeper.py
@@ -1,7 +1,12 @@
 #lets craeate a board object to represent the minesweeper game
 class Board:
-    
-
+    def __init__(self, dim_size, num_bombs):
+        self.dim_size = dim_size
+        self.num_bombs = num_bombs
+        
+        #we'll save (row,col) tuples into this set
+        self.dug = set()
+      
 #play the game
 def play(dim_size = 10, num_bombs = 10):
     pass
