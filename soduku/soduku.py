08c854fcd868ec3285b2a59881fe5edbf7c20319
leonelta <ketchakaren@gmail.com>
2021-08-05 15:52:00 +0200

    finds the next row, col on the puzzle that is not filled

--- soduku/soduku.py
+++ soduku/soduku.py
@@ -1,2 +1,10 @@
-def solve_soduku(puzzle):
+def find_next_empty(puzzle):
+
+def solve_sudoku(puzzle):
+    #solve sudoku using backtracking!
+    #our puzzle is a list of lists, where each inner list is a row inour sudoku puzzle
+    #return whether solution exists
+    #mutates puzzle to be the solution(if solution exists)
     
+    #step 1: choose somewhere onthe puzzle to make a guess
+    row, col = find_next_empty(puzzle)
