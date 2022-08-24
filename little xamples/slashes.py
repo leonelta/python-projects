034591529b48b85766faa44c45fdd4471de65c13
leonelta <ketchakaren@gmail.com>
2022-08-24 19:32:06 +0200

    import features from python argv module to the script

--- little xamples/slashes.py
+++ little xamples/slashes.py
@@ -1,2 +1,14 @@
 tabby_cat = "\tI'm tabbed in."
-persain_cat = "I'm split\n"
+persain_cat = "I'm split\non a line."
+backslash_cat = "I'm  \\ a \\ cat."
+
+fat_cat = """
+I'll do a lisy:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass"""
+
+print(tabby_cat)
+print(persain_cat)
+print(backslash_cat)
+print(fat_cat)
