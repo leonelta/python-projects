2a2e982cc791087ec5e7f182249e40d0382ad73b
leonelta <ketchakaren@gmail.com>
2022-08-24 19:56:21 +0200

    store imput in variable lives

--- little xamples/argu.py
+++ little xamples/argu.py
@@ -9,3 +9,9 @@ print(f"HI {user_name}, I'm the {script} script.")
 print(f"I'd like to ask you a few questions.")
 print(f"Do you like me {user_name}?")
 likes = input(prompt)
+
+print(f"Where do you live {user_name}?")
+lives = input(prompt)
+
+print(f"Do you like me {user_name}?")
+likes = input(prompt)
