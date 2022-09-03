7de3e1a48acf2e0a04514f9a5e6b89936502ef1c
leonelta <ketchakaren@gmail.com>
2022-09-03 02:17:24 +0200

    to close th target file completely

--- little/reandwri.py
+++ little/reandwri.py
@@ -21,3 +21,11 @@ line2 = raw_input("line 2: ")
 line3 = raw_input("line 3: ")
 
 target.write(line1)
+target.write("\n")
+target.write(line2)
+target.write("\n")
+target.write(line3)
+target.write("\n")
+
+print("And finally, we close it")
+target.close()
