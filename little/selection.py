61fe52a2814efbc3cbaa339c488cbb666d15dce3
leonelta <ketchakaren@gmail.com>
2022-09-04 17:58:06 +0200

    using if for the condition if selection is equal to 1 option

---
+++ little/selection.py
@@ -0,0 +1,8 @@
+print("1) Create a new file")
+print("2) Display th file")
+print("2) Add a new subject to the file")
+
+selection = int(input("Make a selection 1, 2 or 3: "))
+
+if selection == 1:
+    subject = input("Enter a school subject: ")
