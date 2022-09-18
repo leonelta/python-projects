6fafa7b2d42f1551dd6bb0415c64107797ad2639
leonelta <ketchakaren@gmail.com>
2022-09-18 18:44:50 +0200

    if people is less than cats another message should be shown

--- little/if.py
+++ little/if.py
@@ -2,5 +2,9 @@ people = 20
 cats = 30
 dogs = 15
 
+if people > cats:
+    print("Too many cats! The world is dommed!")
+    
 if people < cats:
     print("Too many cats! The world is dommed!")
+    
