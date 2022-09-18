501e18db8974fabdf9d5cfc19fae3acc7b64a938
leonelta <ketchakaren@gmail.com>
2022-09-18 20:01:08 +0200

    state the condition if bear == 1 then messages should be displayed

--- little/decisions.py
+++ little/decisions.py
@@ -4,3 +4,8 @@ door = raw_input("> ")
 
 if door == "1":
     print("There's a giant bear eating a cheese cake. What do you do?")
+    print("1. Take the cake")
+    print("2. Scream at the bear")
+    
+    bear = raw_input("> ")
+    if bear == "1":
