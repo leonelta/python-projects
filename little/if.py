a2376d07090a7c249a3e4283fa54b4d0c47665dc
leonelta <ketchakaren@gmail.com>
2022-09-18 18:48:03 +0200

    same comparison is done between people and dogs

--- little/if.py
+++ little/if.py
@@ -3,8 +3,14 @@ cats = 30
 dogs = 15
 
 if people > cats:
-    print("Too many cats! The world is dommed!")
+    print("Too many cats! The world is saved!")
     
 if people < cats:
     print("Too many cats! The world is dommed!")
+    
+if people < dogs:
+    print(" The world is drooled on!")
+    
+if people > dogs:
+    print("The world is dry!")
     
