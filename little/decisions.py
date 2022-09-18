e482184a85b8a552b1ed2a7521a31d0a8bf57a2b
leonelta <ketchakaren@gmail.com>
2022-09-18 19:49:08 +0200

    enter the condition if door == 1 that door #1 the message below should be displayed

---
+++ little/decisions.py
@@ -0,0 +1,6 @@
+print("You enter a dark room with two doors. Do you go throughndorr #1 or door #2")
+
+door = raw_input("> ")
+
+if door == "1":
+    print("There's a giant bear eating a cheese cake. What do you do?")
