a844a0af135313bf2448df583d6889523cf48c8b
leonelta <ketchakaren@gmail.com>
2022-09-23 08:30:45 +0200

    lower return a string where all characters are lower case

--- little/rand.py
+++ little/rand.py
@@ -2,4 +2,6 @@ import random
 colour = random.choice(["red", "blue", "green", "white", "pink"])
 tryagain = true
 while tryagain == True:
+    theirchoice = input("Enter a colour: ")
+    theirchoice = theirchoice.lower()
     
