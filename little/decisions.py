d8a70f0653544d10871d74d2dc97984b72c96019
leonelta <ketchakaren@gmail.com>
2022-09-23 08:23:49 +0200

    import random modules

--- little/decisions.py
+++ little/decisions.py
@@ -26,9 +26,10 @@ elif door == "2":
     if insanity == "1" or insanity == "2":
         print("Your body survives powereed by a mind of jello. Good job")
     else:
-        print("Th insanity rots your eyes into a pool of muck. Good job")
+        print("The insanity rots your eyes into a pool of muck. Good job")
         
 else:
+    print("You stumble around and fall on a knife and die . Good job")
     
         
     
