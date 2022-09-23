77d01ba551057d1e30c95c7c11fc9cacf2af4149
leonelta <ketchakaren@gmail.com>
2022-09-23 08:38:02 +0200

    use if and elif statement for any colour

--- little/rand.py
+++ little/rand.py
@@ -4,4 +4,19 @@ tryagain = true
 while tryagain == True:
     theirchoice = input("Enter a colour: ")
     theirchoice = theirchoice.lower()
+    if colour == theirchoice:
+        print("Well done")
+        tryagain = False
+    else:
+        if colour == "red":
+            print("I bet you are seeing Red right now!")
+        elif colour == "blue":
+            print("Don't feel BLUE")
+        elif colour == "green":
+            print("I bet you are GREEN with envy right now.")
+        elif colour == "white":
+            print("Are you WHITE a sheet, as you didn't guess correctly?")
+        elif colour == "pink":
+            print("Shame you are not feeling in the Pink, as you got it wrong!")
+            
     
