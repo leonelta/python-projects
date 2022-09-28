e9e8b0cd60c73b4d8089d69230eb95a89183d147
leonelta <ketchakaren@gmail.com>
2022-09-28 13:36:26 +0200

    defining a function btnClick with the argument numbers

--- little/calculator.py
+++ little/calculator.py
@@ -1,5 +1,7 @@
 from tkinter import *
 
+def btnClick(numbers):
+
 cal = Tk()
 cal.title("Calculator")
 operator = ""
@@ -8,11 +10,40 @@ text_Input = StringVar()
 txtDisplay = Entry(cal, textvariable=text_Input, font=('arial', 20, 'bold'), bd =30,
                    insertwidth = 4, bg = "powder blue", justify= 'right' ).grid(columnspan = 4)
 
-btn7 = Button(cal, padx=16, fg="black",  font=('arial', 20, 'bold'),
-              text = "7").grid(row =1, column =0)
-btn8 = Button(cal, padx=16, fg="black",  font=('arial', 20, 'bold'),
-              text = "7").grid(row =1, column =1)
-btn9 = Button(cal, padx=16, fg="black",  font=('arial', 20, 'bold'),
-              text = "7").grid(row =1, column =2)
+btn7 = Button(cal, padx=16, pady=16, bd=8, fg="black",  font=('arial', 20, 'bold'),
+              text = "7", bg = "powder blue").grid(row =1, column =0)
+btn8 = Button(cal, padx=16, pady=16, bd=8, fg="black",  font=('arial', 20, 'bold'),
+              text = "8", bg = "powder blue").grid(row =1, column =1)
+btn9 = Button(cal, padx=16, pady=16, bd=8, fg="black",  font=('arial', 20, 'bold'),
+              text = "9", bg = "powder blue").grid(row =1, column =2)
+Addition = Button(cal, padx=16, pady=16, bd=8, fg="black",  font=('arial', 20, 'bold'),
+              text = "+", bg = "powder blue").grid(row =1, column =3)
+
+btn4 = Button(cal, padx=16, pady=16, bd=8, fg="black",  font=('arial', 20, 'bold'),
+              text = "4", bg = "powder blue").grid(row =2, column =0)
+btn5 = Button(cal, padx=16, pady=16, bd=8, fg="black",  font=('arial', 20, 'bold'),
+              text = "5", bg = "powder blue").grid(row =2, column =1)
+btn6 = Button(cal, padx=16, pady=16, bd=8, fg="black",  font=('arial', 20, 'bold'),
+              text = "6", bg = "powder blue").grid(row =2, column =2)
+Subtraction = Button(cal, padx=16, pady=16, bd=8, fg="black",  font=('arial', 20, 'bold'),
+              text = "-", bg = "powder blue").grid(row =2, column =3)
+
+btn1 = Button(cal, padx=16, pady=16, bd=8, fg="black",  font=('arial', 20, 'bold'),
+              text = "1", bg = "powder blue").grid(row =3, column =0)
+btn2 = Button(cal, padx=16, pady=16, bd=8, fg="black",  font=('arial', 20, 'bold'),
+              text = "2", bg = "powder blue").grid(row =3, column =1)
+btn3 = Button(cal, padx=16, pady=16, bd=8, fg="black",  font=('arial', 20, 'bold'),
+              text = "3", bg = "powder blue").grid(row =3, column =2)
+Multiply = Button(cal, padx=16, pady=16, bd=8, fg="black",  font=('arial', 20, 'bold'),
+              text = "*", bg = "powder blue").grid(row =3, column =3)
+
+btn0 = Button(cal, padx=16, pady=16, bd=8, fg="black",  font=('arial', 20, 'bold'),
+              text = "0", bg = "powder blue").grid(row =4, column =0)
+btnClear = Button(cal, padx=16, pady=16, bd=8, fg="black",  font=('arial', 20, 'bold'),
+              text = "C", bg = "powder blue").grid(row =4, column =1)
+btnEquals = Button(cal, padx=16, pady=16, bd=8, fg="black",  font=('arial', 20, 'bold'),
+              text = "=", bg = "powder blue").grid(row =4, column =2)
+Division = Button(cal, padx=16, pady=16, bd=8, fg="black",  font=('arial', 20, 'bold'),
+              text = "/", bg = "powder blue").grid(row =4, column =3)
 
 cal.mainloop()
