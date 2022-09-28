c093bb7758c43af4044d0959b0b32595ee22e0c4
leonelta <ketchakaren@gmail.com>
2022-09-28 10:16:53 +0200

    entry() is used to allow the user to enter some text and it will have the following properties

--- little/calculator.py
+++ little/calculator.py
@@ -4,3 +4,6 @@ cal = Tk()
 cal.title("Calculator")
 operator = ""
 text_Input = StringVar()
+
+txtDisplay = Entry(cal, textvariable=text_Input, font=('arial', 20, bold), bd =30,
+                   insertwidth = 4, bg = "powder blue", justify= 'right' )
