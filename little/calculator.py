6f7fd1b6c3fdc631609719c3c8e29827e32c259c
leonelta <ketchakaren@gmail.com>
2022-09-28 12:00:02 +0200

    create a button with the options below and attributing it a position using grid

--- little/calculator.py
+++ little/calculator.py
@@ -8,4 +8,6 @@ text_Input = StringVar()
 txtDisplay = Entry(cal, textvariable=text_Input, font=('arial', 20, 'bold'), bd =30,
                    insertwidth = 4, bg = "powder blue", justify= 'right' ).grid(columnspan = 4)
 
+btn7 = Button(cal, padx=16, fg="black",  font=('arial', 20, 'bold'),
+              text = "7").grid(row =1, column =0)
 cal.mainloop()
