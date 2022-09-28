b0d6ccc7875ce67097f5063484d22b8b7756dbc0
leonelta <ketchakaren@gmail.com>
2022-09-28 11:49:10 +0200

    it is methhod usdd when the application is ready to run

--- little/calculator.py
+++ little/calculator.py
@@ -5,5 +5,7 @@ cal.title("Calculator")
 operator = ""
 text_Input = StringVar()
 
-txtDisplay = Entry(cal, textvariable=text_Input, font=('arial', 20, bold), bd =30,
+txtDisplay = Entry(cal, textvariable=text_Input, font=('arial', 20, 'bold'), bd =30,
                    insertwidth = 4, bg = "powder blue", justify= 'right' ).grid(columnspan = 4)
+
+cal.mainloop()
